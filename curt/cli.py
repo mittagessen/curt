bc5d7925de5623fa7efcf63632eb59efa08a8f0b
Benjamin Kiessling <mittagessen@l.unchti.me>
2022-03-17 13:06:11 +0100

    save best 5 models during training

--- curt/cli.py
+++ curt/cli.py
@@ -13,8 +13,9 @@ from PIL import Image
 from pathlib import Path
 from rich.logging import RichHandler
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-from curt.models import DetrCurveModel, CurtCurveModel
+from curt.models import CurtCurveModel
 from curt.dataset import CurveDataModule
 from curt.progress import KrakenTrainProgressBar
 
@@ -176,9 +177,9 @@ def train(ctx, learning_rate, learning_rate_backbone, batch_size, weight_decay,
                                   num_workers=workers)
 
     if load:
-        model = DetrCurveModel.load_from_checkpoint(load)
+        model = CurtCurveModel.load_from_checkpoint(load)
     else:
-        model = DetrCurveModel(data_module.num_classes+1,
+        model = CurtCurveModel(data_module.num_classes+1,
                                num_queries=num_queries,
                                learning_rate=learning_rate,
                                weight_decay=weight_decay,
@@ -192,11 +193,13 @@ def train(ctx, learning_rate, learning_rate_backbone, batch_size, weight_decay,
                                num_heads=num_heads,
                                dim_ff=dim_ff)
 
+    checkpoint_cb = ModelCheckpoint(monitor='loss', save_top_k=5, mode='min')
+
     trainer = Trainer(default_root_dir=output,
                       gradient_clip_val=clip_max_norm,
                       max_epochs=epochs,
                       gpus=device,
-                      callbacks=[KrakenTrainProgressBar()],
+                      callbacks=[KrakenTrainProgressBar(), checkpoint_cb],
                       **val_check_interval)
 
     trainer.fit(model, data_module)
