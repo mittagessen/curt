ed9bc2ef863bf36a0595576e13929bbcd779729a
Benjamin Kiessling <mittagessen@l.unchti.me>
2022-04-27 14:51:15 +0200

    make auxiliary losses default

--- curt/cli.py
+++ curt/cli.py
@@ -253,7 +253,7 @@ def polytrain(ctx, precision, learning_rate, batch_size, weight_decay, epochs, f
               default=False,
               help='Merge all baseline types into `default`')
 @click.option('--set-matcher/--dummy-matcher', show_default=True, default=True, help='Use the set criterion or dummy matching.')
-@click.option('--aux-loss/--no-aux-loss', show_default=True, default=False, help='Enable auxiliary losses in decoder.')
+@click.option('--aux-loss/--no-aux-loss', show_default=True, default=True, help='Enable auxiliary losses in decoder.')
 @click.option('--workers', show_default=True, default=2, help='Number of data loader workers.')
 @click.option('-d', '--device', show_default=True, default='1', help='Select device to use')
 @click.argument('ground_truth', nargs=-1, callback=_expand_gt, type=click.Path(exists=False, dir_okay=False))
